9ae33f4615bd0b2a35cb4dc598a0d9c7b5c2c841
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-10-15 12:02:22 +0530

    index render updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/App';
+import AppRoute from './AppRoute';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppRoute />, document.getElementById('root'));
 registerServiceWorker();
