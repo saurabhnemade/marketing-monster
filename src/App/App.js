8811def40b4dd22e27d064d66269365e7f9424b5
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-10-15 12:01:43 +0530

    routing removed from appjs

--- src/App/App.js
+++ src/App/App.js
@@ -1,30 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-          <a className="navbar-brand" href="#">MMonster</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">Templates</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">Editor</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        app
       </div>
     );
   }
