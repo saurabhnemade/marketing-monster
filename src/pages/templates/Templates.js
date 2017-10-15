0dc4b6d4b200ac81021b0feb65521a13aac833ba
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-10-15 12:04:10 +0530

    Templates page added

---
+++ src/pages/templates/Templates.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Templates extends Component {
+    render() {
+        return (
+            <div>
+                Templates page
+            </div>
+        );
+    }
+}
