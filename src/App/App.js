ca5174504d6ff415ea28a031ca9aff6c00ba7e26
saurabhnemade <saurabhnemade@users.noreply.github.com>
2017-10-15 11:19:42 +0530

    class name corrected

--- src/App/App.js
+++ src/App/App.js
@@ -5,22 +5,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-          <a class="navbar-brand" href="#">MMonster</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+          <a className="navbar-brand" href="#">MMonster</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">Templates</a>
+              <li className="nav-item">
+                <a className="nav-link">Templates</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">Editor</a>
+              <li className="nav-item">
+                <a className="nav-link">Editor</a>
               </li>
             </ul>
           </div>
